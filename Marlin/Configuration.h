5596296e6a05cf3ca1e31c6da7499ca9becb63ca
Daniel Ashbrook <dan@danielashbrook.com>
2023-10-11 18:18:13 +0200

    Revert accidental e-steps change

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1235,7 +1235,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 52.398, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 52.398, 93 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
